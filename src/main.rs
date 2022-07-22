745dc73fb691f46bd1874877a4d7bcde2cc58f7d
fabiokleis <fabiohkrc@gmail.com>
2022-07-22 19:05:43 -0300

    created a  to the game and handle inputs

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,142 @@
 #![allow(dead_code)]
 use std::char;
+use std::collections::HashMap;
 use std::io::{self, Write};
+use std::fmt;
 
 const WIDTH: usize = 20;
 const HEIGHT: usize = 10;
 
-type Cell = Vec<Vec<char>>;
+#[derive(Clone, Copy)]
+enum Commands {
+    W,
+    A,
+    S,
+    D,
+    Clear,
+    History,
+    Reset,
+}
 
-#[derive(Debug)]
+impl fmt::Display for Commands {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Commands::W => write!(f, "W"),
+            Commands::A => write!(f, "A"),
+            Commands::S => write!(f, "S"),
+            Commands::D => write!(f, "D"),
+            Commands::Clear => write!(f, "CLEAR"),
+            Commands::Reset => write!(f, "RESET"),
+            Commands::History => write!(f, "HISTORY"),
+        }
+    }
+}
+
+#[derive(Clone)]
 struct Board {
     width: usize,
     height: usize,
-    matrix: Cell,
+    commands: HashMap<String, Commands>,
+    history: Vec<String>,
+    matrix: Vec<Vec<char>>,
 }
-
 
 impl Board {
     fn new(width: usize, height: usize) -> Self {
-
         Board {
             width,
             height,
-            matrix: vec![vec!['.'; width]; height]
+            commands: HashMap::<String, Commands>::new(),
+            history: Vec::<String>::new(),
+            matrix: vec![vec!['.'; width]; height],
         }
     }
 
+    // show the current state of matrix
     fn show(&self) {
         for i in self.matrix.iter() {
             for j in i.iter() {
                 print!("{}", j);
-            } 
+            }
             println!("");
+        }
+    }
+
+    /// capture and match every command type
+    fn handle_command(&mut self, cmd: String) {
+        let cmd = cmd.trim().to_string();
+        self.history.push(cmd.clone());
+        match self.commands.get(&cmd) {
+            Some(Commands::W) => todo!(),
+            Some(Commands::A) => todo!(),
+            Some(Commands::S) => todo!(),
+            Some(Commands::D) => todo!(),
+            Some(Commands::History) => self.show_history(),
+            Some(Commands::Clear) => self.clear_term(),
+            _ => {}
+        }
+    }
+
+    /// show history vector elements
+    fn show_history(&self) {
+        for (i, cmd) in self.history.iter().enumerate() {
+            println!("{} {}", i, cmd);
+        }
+    }
+
+    /// clear terminal stdout
+    fn clear_term(&self) {
+        std::process::Command::new("clear").status().expect("cannot clear terminal!");
+    }
+
+    fn populate_commands(&mut self) {
+        self.commands = HashMap::from([
+            (String::from("w"), Commands::W),
+            (String::from("a"), Commands::A),
+            (String::from("s"), Commands::S),
+            (String::from("d"), Commands::D),
+            (String::from("clear"), Commands::Clear),
+            (String::from("history"), Commands::History),
+            (String::from("reset"), Commands::Reset),
+        ]);
+    }
+
+    fn clear_board(&mut self) {
+        for i in self.matrix.iter_mut() {
+            for j in i.iter_mut() {
+                *j = '.';
+            }
         }
     }
 }
 
 /// game loop
 fn run(board: &mut Board) {
-    let mut quit = false;
+
+    board.populate_commands();
     let mut input = String::new();
 
-    while !quit {
-
-        // clear terminal stdout
-        std::process::Command::new("clear").status().unwrap();
+    'running: loop {
+        
         board.show();
-
-        // expect a command
         print!("> ");
         io::stdout().flush().expect("cannot flush stdout!");
 
-        // match the possibles commands
+        // match the possible commands
         match io::stdin().read_line(&mut input) {
             Ok(_line) => {
                 match input.trim() {
-                    "q" => quit = true,
-                    _ => {}
-                }
+                    "q" | "quit" => break 'running,
+                    _ => {
+                        if board.commands.contains_key(input.trim()) {
+                            board.handle_command(input.clone());
+                        }
+                    },
+                } 
             },
-            Err(error) => println!("error: {error}"),
+            Err(_) => {}
         }
-        
+
         // clear input string
         input.clear();
     }
